c3e658fe7d37e64ac9de549d7f7f18b407b0a25b
Bozar <tommyfranklin42@outlook.com>
2018-02-27 16:59:14 +0800

    create a canvas

--- scripts/game.js
+++ scripts/game.js
@@ -1,30 +1,24 @@
 'use strict'
 
-// http://www.codingcookies.com/2013/04/01/building-a-roguelike-in-javascript-part-1/
+var Game = {}
+Game.version = '0.0.1-dev'
+Game.display = new ROT.Display({ width: 50, height: 28, fontSize: 22 })
+
+Game.upper = function (text) {
+  return text.toUpperCase()
+}
 
 window.onload = function () {
-  // Check if rot.js can work on this browser
   if (!ROT.isSupported()) {
-    window.alert("The rot.js library isn't supported by your browser.")
-  } else {
-      // Create a display 80 characters wide and 20 characters tall
-    var display = new ROT.Display({width: 80, height: 20})
-    var container = display.getContainer()
-      // Add the container to our HTML page
-    document.body.appendChild(container)
-    var foreground, background, colors
-    for (var i = 0; i < 15; i++) {
-          // Calculate the foreground color, getting progressively darker
-          // and the background color, getting progressively lighter.
-      foreground = ROT.Color.toRGB([255 - (i * 20),
-        255 - (i * 20),
-        255 - (i * 20)])
-      background = ROT.Color.toRGB([i * 20, i * 20, i * 20])
-          // Create the color format specifier.
-      colors = '%c{' + foreground + '}%b{' + background + '}'
-          // Draw the text two columns in and at the row specified
-          // by i
-      display.drawText(2, i, colors + 'Hello, world!')
-    }
+    window.alert('rot.js is not supported by your browser.')
+    return
   }
+  document.getElementById('game').appendChild(Game.display.getContainer())
+
+  Game.display.drawText(5, 3, '1234567890', '#619DD8')
+  Game.display.draw(5, 4, '@', '#619DD8', 'red')
+  Game.display.drawText(5, 5, '%c{green}hello%c{} world,')
+  Game.display.drawText(5, 6,
+    `%c{yellow}%b{grey}great%b{} ${Game.upper('hero')}%c{}`, 6)
+  Game.display.drawText(4, 26, '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0', 20)
 }
